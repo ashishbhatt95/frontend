a97a433eaf93f8ac126afc77adc472c7325f2994
RakeshKumarBijarniya <rakeshbijarniya784@gmail.com>
2025-09-26 23:06:36 +0530

    change in height

--- src/authVendorComp/VendorAddProduct.js
+++ src/authVendorComp/VendorAddProduct.js
@@ -1355,7 +1355,7 @@ const VendorAddProduct = () => {
 const styles = {
   mainContainer: {
     width: "100%",
-    height: "100vh",
+    height: "100%",
     backgroundColor: "#E8EFFF",
     padding: "20px 10px 10px 30px",
   },
